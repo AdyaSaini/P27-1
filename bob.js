127185310242a101080e543867bff3052f157f68
Adya <71136712+AdyaSaini@users.noreply.github.com>
2020-12-02 21:11:07 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,18 @@
+class bob {
+    constructor(x, y) {
+        var options= {
+            'isStatic' : true,
+            'restitution':1.4,
+            'friction':0.3,
+            'density':0.1
+        }
+        this.body = Bodies.circle(x, y, 10, options); 
+        World.add(world, this.body);
+    }
+    display(){
+        var pos =this.body.position;
+        translate(pos.x, pos.y);
+        fill('#ff66b8');
+        ellipse(0,0,20,20);
+    }
+}
